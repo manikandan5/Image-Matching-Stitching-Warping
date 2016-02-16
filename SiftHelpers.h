9a4cc5815249f764e3a19ac94ac8a487f8a40d8a
Suhas Gulur Ramakrishna <suhgulur@hulk.soic.indiana.edu>
2016-02-16 00:32:52 -0500

    Sift ratio distance function

--- SiftHelpers.h
+++ SiftHelpers.h
@@ -18,6 +18,7 @@ class Image
 {	
 	string name;
 	vector<SiftDescriptor> descriptors;
+	CImg<double> input_image;
 	
 	public:
 	
@@ -25,6 +26,7 @@ class Image
 	{
 		name = Name;
 		CImg<double> imageData(name.c_str());
+		input_image = imageData;
 		CImg<double> gray = imageData.get_RGBtoHSI().get_channel(2);
 		descriptors = Sift::compute_sift(gray);
 	}
@@ -33,6 +35,20 @@ class Image
 	{
 		return descriptors;
 	}
+	
+	CImg<double> getImageData() const
+	{
+		return input_image;
+	}
+	
+	struct SiftDescriptorCompare
+	{
+		bool operator() (const SiftDescriptor& lhs, const SiftDescriptor& rhs) const
+		{
+			return lhs.row < rhs.row;
+		}
+	};
+
 
 	static void MatchSIFT(const Image& queryImage,const Image& image)
 	{
@@ -41,9 +57,13 @@ class Image
 		
 		double distance = 0.0;
 		
+		map<SiftDescriptor,SiftDescriptor,SiftDescriptorCompare> mapping;
+		int count = 0;
+			
 		for(int i = 0;i<queryDescriptors.size();++i)
 		{
 			double min = DBL_MAX;
+			double second_min = DBL_MAX;
 			int descriptor = -1;
 			for(int j =0;j<imageDescriptors.size();++j)
 			{
@@ -52,16 +72,36 @@ class Image
 				{
 					sum+= abs(queryDescriptors[i].descriptor[k] - imageDescriptors[j].descriptor[k]);
 				}
+				
+				sum = sqrt(sum);
 				if(sum < min)
 				{
+					second_min = min;
 					min = sum;
 					descriptor = j;
 				}
+				else if(sum < second_min)
+				{
+					second_min = sum;
+				}
 			}
 			
+			if(min/second_min < 0.8)
+			{
+				count++;
+				cout<<min<<",";
+			
+				SiftDescriptor q = queryDescriptors[i];
+				SiftDescriptor im = imageDescriptors[descriptor];
+				mapping[q] = im;
+			}
 			distance+=min;
 		}
+		cout<<"Count = "<<count<<endl;
 		
+		CImg<double> queryImageData = queryImage.getImageData();
+		queryImageData.append(image.getImageData());
+		queryImageData.get_normalize(0,255).save("sift.png");
 		cout<<distance<<endl;
 	}
 	
